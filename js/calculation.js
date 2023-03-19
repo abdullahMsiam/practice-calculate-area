4bf319e294df21271da5bcb272da8cadee17f5ec
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2023-03-19 22:17:39 +0600

    js calculation

---
+++ js/calculation.js
@@ -0,0 +1,60 @@
+document.getElementById('triangle-cal-btn').addEventListener('click', function () {
+    const actualInput1 = getInputValue('triangle-input-1');
+
+    const actualInput2 = getInputValue('triangle-input-2');
+    if (isNaN(actualInput1) || isNaN(actualInput2) || actualInput1 < 0 || actualInput2 < 0) {
+        alert('You have to provide two number');
+    }
+
+    else {
+        const triangleArea = (0.5 * actualInput1 * actualInput2);
+
+        addedElement('Triangle Area:  ', triangleArea);
+    }
+
+
+
+})
+
+document.getElementById('rectangle-cal-btn').addEventListener('click', function () {
+    const actualInput1 = getInputValue('rectangle-input-1');
+
+    const actualInput2 = getInputValue('rectangle-input-2')
+
+    if (isNaN(actualInput1) || isNaN(actualInput2) || actualInput1 < 0 || actualInput2 < 0) {
+        alert('You have to provide two number');
+    }
+
+    else {
+        const rectangleArea = (actualInput1 * actualInput2);
+        addedElement('Rectangle Area:  ', rectangleArea);
+    }
+
+
+})
+
+document.getElementById('parallelogram-cal-btn').addEventListener('click', function () {
+    const actualInput1 = getInputValue('parallelogram-input-1');
+    const actualInput2 = getInputValue('parallelogram-input-2');
+
+    if (isNaN(actualInput1) || isNaN(actualInput2) || actualInput1 < 0 || actualInput2 < 0) {
+        alert('You have to provide two number');
+    }
+    else {
+        const parallelogramArea = actualInput1 * actualInput2;
+        addedElement('Parallelogram:  ', parallelogramArea);
+    }
+
+})
+
+document.getElementById('rhombus-cal-btn').addEventListener('click', function () {
+    const actualInput1 = getInputValue('rhombus-input-1');
+    const actualInput2 = getInputValue('rhombus-input-2');
+    if (isNaN(actualInput1) || isNaN(actualInput2) || actualInput1 < 0 || actualInput2 < 0) {
+        alert('You have to provide two number');
+    }
+    else {
+        const rhombusArea = (0.5 * actualInput1 * actualInput2);
+        addedElement('Rhombus Area:  ', rhombusArea);
+    }
+})
---
+++ js/ulity.js
@@ -0,0 +1,15 @@
+function getInputValue(fieldInputId) {
+    const input = document.getElementById(fieldInputId);
+    const inputStr = input.value;
+    const actualInput = parseFloat(inputStr);
+    return actualInput;
+}
+
+//  added item 
+function addedElement(areaName, calculateValue) {
+    const li = document.createElement('li');
+    const node = document.createTextNode(areaName + calculateValue);
+    li.appendChild(node);
+    const addedItem = document.getElementById('added-item');
+    addedItem.appendChild(li);
+}
